a84800e44d33859da3977e2ab51c9d8bc2d4238a
Cbuapim <ch252655@ucf.edu>
2025-02-04 23:04:33 -0500

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>A Young Boy with a Magical Gift</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+// src/components/Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Kubo and Monsters</h1>
+    </header>
+  );
+};
+
+export default Header;
